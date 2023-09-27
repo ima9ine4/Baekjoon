593523b9519e359501f52a4093c762eff076df53
HYEJIN LIM <105336619+ima9ine4@users.noreply.github.com>
2023-09-27 15:50:21 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 4920 ms, Memory: 62384 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,26 @@
+import heapq
+
+v,e = map(int, input().split())
+arr = []
+visited =[]
+answer = 0;
+for _ in range(v+1):
+    arr.append([])
+    visited.append(0)
+
+for _ in range(e):
+    a,b,c = map(int,input().split())
+    arr[a].append([c,b])
+    arr[b].append([c,a])
+
+heap = [[0,1]]
+while heap:
+    c,b = heapq.heappop(heap)
+    if visited[b] !=0:
+        continue
+    else:
+        visited[b]=1
+        answer +=c 
+        for tmp in arr[b]:
+            heapq.heappush(heap,tmp)
+print(answer)
