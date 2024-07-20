423f7c5975c8a73bbf3ba6210d9ca496b35e1b4e
HYEJIN LIM <105336619+ima9ine4@users.noreply.github.com>
2024-07-20 14:20:52 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -2,32 +2,33 @@
 #include <cstring>
 using namespace std;
 
-#define MAX_N 101
-int V,E;
-int cnt = 0;
-int graph[MAX_N][MAX_N];
-bool visited[MAX_N];
+#define MAX_V 101
 
-void dfs(int node){
-    visited[node] = true;
-    for(int next = 1; next <= V; next++){
-        if(!visited[next] && graph[node][next]){
-            cnt += 1;
-            dfs(next);
+int V,E;
+int graph[MAX_V][MAX_V];
+bool visited[MAX_V];
+int cnt = 0;
+
+void dfss(int v){
+    visited[v] = true;
+    
+    for(int next=1;next<=V;next++){
+        if(!visited[next] && graph[v][next]){
+            cnt++;
+            dfss(next);
         }
     }
 }
 
 int main() {
     cin>>V>>E;
-    memset(visited, 0, sizeof(visited));
+    memset(visited,0,sizeof(visited));
     memset(graph, 0, sizeof(graph));
-    for(int i=0;i<E;i++){
-        int a,b;
-        cin >> a >> b;
-        graph[a][b] = graph[b][a] = 1;
+    for(int i=0;i<E;i++) {
+        int a,b; cin>>a>>b;
+        graph[a][b] = graph[b][a] = 1; 
     }
-    dfs(1);
-    cout << cnt << endl;
+    dfss(1);
+    cout<<cnt<<endl;
     return 0;
 }
