d0fde1dcb424d4f993b8c44860d639b0eac9b374
HYEJIN LIM <105336619+ima9ine4@users.noreply.github.com>
2024-08-17 09:49:58 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int t; cin>>t;
+    t = t/4;
+    for(int i=0;i<t;i++) {
+        cout<<"long"<<' ';
+    }
+    cout<<"int"<<endl;
+    return 0;
+}
