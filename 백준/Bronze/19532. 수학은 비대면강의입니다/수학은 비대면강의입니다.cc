e54ed5bac7fd0ec24ec530f4a2aff5e128cb7eeb
HYEJIN LIM <105336619+ima9ine4@users.noreply.github.com>
2024-08-18 22:39:59 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int a,b,c,d,e,f; cin>>a>>b>>c>>d>>e>>f;
+    int x = (c*e - b*f)/(a*e - b*d);
+    int y = (c*d - f*a)/(b*d - a*e);
+    cout<<x<<' '<<y<<endl;
+    return 0;
+}
