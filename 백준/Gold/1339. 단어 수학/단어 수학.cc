a961f647c19fa0b45ac4ad0ebed3704f8cddad68
HYEJIN LIM <105336619+ima9ine4@users.noreply.github.com>
2025-01-04 14:40:48 +0900

    [Gold IV] Title: 단어 수학, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.cc
+++ 백준/Gold/1339. 단어 수학/단어 수학.cc
@@ -1,35 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
+
 int main(){
     int n; cin>>n;
-    vector<string> strs(n);
     vector<int> alpha(26,0);
-
+    string tmp;
     for(int i=0;i<n;i++){
-        cin>>strs[i];
-    }
-    
-    for(int i=0;i<n;i++){//n개의 단어 순회
+        cin>>tmp;
         int pow = 1;
-        for(int j=strs[i].size()-1;j>=0;j--){//한 단어에서의 알파벳 순회
-               alpha[strs[i][j]-'A'] += pow;
-               pow *= 10;
+        for(int j=tmp.size()-1;j>=0;j--){
+            alpha[tmp[j]-'A'] += pow;
+            pow *= 10;
         }
     }
     sort(alpha.begin(),alpha.end());
-    
     int num = 9;
     int ans = 0;
-    for(int i=25;i>=0;i--){
+    for(int i=alpha.size()-1;i>=0;i--){
         if(alpha[i]==0) break;
-        ans += alpha[i] * num;
+        ans += (alpha[i]*num);
         num--;
     }
-
     cout<<ans<<endl;
-
-    return 0;
 }
